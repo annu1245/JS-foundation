e8be37ac3a48e5634cc1e8c8ff503264ffa2cd20
Annu <annupoddar444@gmail.com>
2023-09-03 00:07:58 +0530

    next button functionality added

--- cashier/cashier.js
+++ cashier/cashier.js
@@ -3,25 +3,35 @@ const cashGiven = document.getElementById('cash-given');
 const checkBtn = document.getElementById('check');
 const message = document.getElementById('message');
 const numOfNotes = document.querySelectorAll('.num-of-notes');
+const nextBtn = document.getElementById("next-btn");
+const tableSection = document.querySelector(".cash-count");
+const cashLabel = document.getElementById("cash-label");
+
+HTMLInputElement.prototype.val = function () { 
+    return parseInt(this.value);
+};
+
 const notes = [2000,500,100,20,10,5,1];
 
 checkBtn.addEventListener("click", function validateAmount() {
     //initially hide any message
     message.style.display = 'none';
+    tableSection.style.display = 'block';
     //check the bill amount's value should be positive
-    if(billAmount.value > 0){
+   
+    if(billAmount.val() > 0) {
         //check the given cash should be greater than bill amount 
-        if(cashGiven.value >= billAmount.value){
+        if(cashGiven.val() >= billAmount.val()) {
             // calculate returnable cash amount
-            const returnAmount = cashGiven.value - billAmount.value;
+            const returnAmount = cashGiven.val() - billAmount.val();
             console.log(returnAmount);
             //function for display number of notes to be return
             checkReturnAmount(returnAmount);
-        }else{
-            showMessage("unsufficient cash given")
+        } else {
+            showMessage("Insufficient cash given")
         }
     }
-    else{
+    else {
         //error message
         showMessage("Invalid amount");
     }
@@ -46,4 +56,25 @@ function showMessage(msg) {
     //the error message will be display in the html tag
     message.innerText = msg;
 }
+
+window.onload = function() {
+    tableSection.style.display = 'none';
+    cashGiven.style.display = 'none';
+    // document.querySelector(`label[for=${cashGiven.id}]`).style.display = 'none';
+    cashLabel.style.display = 'none';
+    checkBtn.style.display = 'none';
+};
+
+nextBtn.addEventListener('click', function showSection() {
+    let bill = parseInt(billAmount.value);
+   if(!billAmount.value) {
+        showMessage("enter some amount");
+   } else {
+        cashLabel.style.display = 'block';
+        cashGiven.style.display = 'block';
+        checkBtn.style.display = 'block';
+        nextBtn.style.display = 'none';
+   }
+})
+
 
