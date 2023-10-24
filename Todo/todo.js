f4f4d62415e9c348bc4edef69b598676f2ec11ca
Annu Poddar <annupoddar444@gmail.com>
2023-10-24 21:22:53 +0530

    Added todo using javascript

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,22 +1,42 @@
 submintBtn = document.getElementById("submit");
-list_div = document.querySelector("#list");
+listDiv = document.querySelector("#list");
 text = document.querySelector('input');
-
+testbtn = document.getElementById('test');
 
 submintBtn.addEventListener("click", function() {
-    elemDiv = document.createElement("div");
-    elemDiv.classList.add("elem");
+    if(text.value != '') {
+        
+        elemDiv = document.createElement('div');
+        elemDiv.classList.add('elem');
 
-    ptag = document.createElement("p");
-    ptag.innerText = text.value;
+        textDiv = document.createElement('spam');
+        textDiv.classList.add('text');
+        textDiv.innerText = text.value;
 
-    iconDiv = document.createElement('div');
-    iconDiv.classList.add("icons");
-    
-    elemDiv.appendChild(ptag);
-    elemDiv.appendChild(iconDiv);
-    list_div.appendChild(elemDiv);
-    text.value = "";
+        icon1 = document.createElement('spam');
+        icon1.classList.add('icon1');
+        icon1.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+
+        icon2 = document.createElement('spam');
+        icon2.classList.add('icon2');
+        icon2.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+
+        elemDiv.appendChild(textDiv);
+        elemDiv.appendChild(icon1);
+        elemDiv.appendChild(icon2);
+        listDiv.appendChild(elemDiv);
+
+        text.value = "";
+    }else {
+        alert("please write your todo")
+    }
 })
 
 
+document.querySelectorAll('.icon2').forEach((icon2) => {
+    icon2.addEventListener('click', (item) => {
+        console.log(item.target.parentElement);
+        test = item.target.parentElement;
+        test.remove();
+      });
+})
