287f8e8366196e149b853716007b065374d67f10
Annu Poddar <annupoddar444@gmail.com>
2023-10-31 15:07:03 +0530

    localstorage storing techinque changed and update todo function added

--- Todo/todo.js
+++ Todo/todo.js
@@ -4,13 +4,16 @@ text = document.querySelector('input');
 testbtn = document.getElementById('test');
 clearBtn = document.getElementById('clear_btn');
 
+//click Event on Submit Button
 submintBtn.addEventListener("click", function() {
-    input_val = text.value.trim();
-    if(input_val != '') {
-        addDiv(input_val);
-        setLocal(input_val);
-        removeDiv();
-        textStripe();
+    //get the index for the next div
+    let todoIndex = listDiv.getElementsByClassName('elem').length;
+    todoIndex.length == 0 ? clearBtn.style.display = 'none' :  clearBtn.style.display = 'block';
+    input_val = text.value.trim(); //trim the extra spaces
+    if (input_val != '') {
+        addDiv(input_val, todoIndex);
+        setLocal(input_val, todoIndex);
+        addListeners();
         text.value = '';
     } 
     else {
@@ -18,91 +21,119 @@ submintBtn.addEventListener("click", function() {
     }
 })
 
-clearBtn.addEventListener("click", function() {
-    localStorage.clear();
-    alert("all cleared");
-    location.reload();
-})
+function addDiv(input_val, todoIndex) {
+    elemDiv = document.createElement('div');
+    elemDiv.classList.add('elem', 'new-box');
+    elemDiv.setAttribute('index', todoIndex);
 
-function setLocal(data) {
-    let myTodoData = JSON.parse(localStorage.getItem("mytodo")) || [];
-    myTodoData.push(data);
+    textDiv = document.createElement('textarea');
+    textDiv.classList.add('check-area');
+    textDiv.setAttribute('readonly', 'readonly');
+    textDiv.innerText = input_val;
+
+    edit = document.createElement('button');
+    edit.classList.add('edit');
+    edit.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+
+    del = document.createElement('button');
+    del.classList.add('del');
+    del.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+
+    elemDiv.appendChild(textDiv);
+    elemDiv.appendChild(edit);
+    elemDiv.appendChild(del);
+    listDiv.appendChild(elemDiv);
+    setTimeout(() => elemDiv.classList.remove('new-box'), 300);
+}
+
+//add Listeners to the edit and delete button
+function addListeners() {
+    document.querySelectorAll('.edit').forEach((editDiv) => addEditListener(editDiv));
+    document.querySelectorAll('.del').forEach((delDiv) => addDeleteListener(delDiv));
+}
+
+//Edit Event Listener
+function addEditListener(editElement) {
+    editElement.addEventListener('click', (element) => {
+        let textArea = element.target.parentElement.firstChild;
+        //check either clicked to edit the todo or edit is done
+        let = isEditable = textArea.getAttribute('readonly');
+        if(isEditable !== null) {
+            // edit permission -> remove readonly and display textarea to edit
+            element.target.innerHTML = '<i class="fa-regular fa-circle-check">';
+            textArea.removeAttribute('readonly');
+            textArea.style.background = "white";
+            textArea.focus();
+            textArea.selectionStart = textArea.value.length;
+        }
+        else{
+            // done update -> set readonly and remove the editable textarea
+            textArea.setAttribute('readonly', 'readonly');
+            textArea.style.background = "yellow";
+            element.target.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+            let elemIndex = element.target.parentElement.getAttribute('index');
+            updateLocal(textArea.value, elemIndex); 
+
+        }
+    })
+}
+
+//delete one todo which is clicked
+function addDeleteListener(delElement) {
+    delElement.addEventListener("click", (element) => {
+        let index = element.target.parentElement.getAttribute('index');
+        let elementDiv = element.target.parentElement;
+        //add the removed-item class for the animation while deleting todo
+        elementDiv.classList.add('removed-item');
+        setTimeout(() => elementDiv.remove(), 1000);
+        removeLocal(index);
+    })
+}
+
+// ---------- ----------- LocalStorage ---------------- ------------//
+
+function setLocal(data, index) {
+    let myTodoData = JSON.parse(localStorage.getItem("mytodo")) || {};
+    myTodoData[index] = data;
     localStorage.setItem("mytodo", JSON.stringify(myTodoData));
 }
 
 function getLocal() {
-    var myTodoData = JSON.parse(localStorage.getItem("mytodo")) || [];
-    if(myTodoData.length < 0){
+    let myTodoData = JSON.parse(localStorage.getItem("mytodo")) || {};
+    if (myTodoData === null || Object.keys(myTodoData).length === 0) {
+        listDiv.innerHTML = '';
+        clearBtn.style.display = 'none';   
         return;
-    }else {
-        myTodoData.forEach((item)=>{
-            console.log("item" + item);
-            addDiv(item);
-        })
+    } else {
+        for (var key in myTodoData) {
+            if (myTodoData.hasOwnProperty(key)) {
+              var val = myTodoData[key];
+              //show all data of localStorage
+              addDiv(val, key);
+            }
+          }
     }
 }
 
-
-function addDiv(input_val) {
-    elemDiv = document.createElement('div');
-    elemDiv.classList.add('elem', 'new-box');
-
-    textDiv = document.createElement('spam');
-    textDiv.classList.add('text');
-    textDiv.innerText = input_val;
-
-    icon1 = document.createElement('spam');
-    icon1.classList.add('icon1');
-    icon1.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
-
-    icon2 = document.createElement('spam');
-    icon2.classList.add('icon2');
-    icon2.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-
-    elemDiv.appendChild(textDiv);
-    elemDiv.appendChild(icon1);
-    elemDiv.appendChild(icon2);
-    listDiv.appendChild(elemDiv);
-
-    setTimeout(() => {
-        elemDiv.classList.remove('new-box');
-    }, 300)
+function updateLocal(updatedText, index) {
+    let myTodoData = JSON.parse(localStorage.getItem("mytodo"));
+    myTodoData[index] = updatedText;
+    localStorage.setItem("mytodo", JSON.stringify(myTodoData));
 }
 
-function removeDiv() {
-    let myTodoData = JSON.parse(localStorage.getItem("mytodo")) || [];
-    document.querySelectorAll('.icon2').forEach((icon2) => {
-        icon2.addEventListener('click', (item) => {
-            let itemVal = item.target.parentNode.firstChild.innerText;
-            if(myTodoData.includes(itemVal)) {
-                removeTodo(itemVal, myTodoData);
-            }
-            test = item.target.parentElement;
-            test.classList.add('removed-item')
-            setTimeout(() => {
-                test.remove();
-            },1000)
-          });
-    });
-    
+function removeLocal(index) {
+    let myTodoData = JSON.parse(localStorage.getItem('mytodo'));
+    delete myTodoData[index];
+    localStorage.setItem('mytodo', JSON.stringify(myTodoData));
 }
 
-function textStripe() {
-    document.querySelectorAll('.icon1').forEach((icon1) => {
-        icon1.addEventListener('click', (el) => {
-            fc = el.target.parentNode.firstChild;
-            fc.classList.add('textEffect');
-          });
-    });
-}
-
-function removeTodo(itemVal, myTodoData) {
-    localStorage.removeItem(itemVal);
-    index = myTodoData.indexOf(itemVal);
-    myTodoData.splice(index, 1);
-    localStorage.setItem('mytodo',JSON.stringify(myTodoData));
-}
+clearBtn.addEventListener("click", function() {
+    localStorage.clear();
+    // location.reload();
+    getLocal();
+})
 
 window.onload = (event) =>{
     getLocal();
+    addListeners();
 };
