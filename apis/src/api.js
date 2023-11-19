7e423dd01c903ad063a90ac0880cad7b8bb28578
Annu Poddar <annupoddar444@gmail.com>
2023-11-19 10:28:44 +0530

    router added to api.js

--- apis/src/api.js
+++ apis/src/api.js
@@ -6,9 +6,17 @@ const apiData = require("./assets/data.json");
 const app = express();
 app.use(cors());
 
-app.get("/api/get-quiz-questions", (req, res) => {
-    return res.json(apiData);
-});
+const router = express.Router();
+
+router.get("/get-quiz-questions", (req, res) => {
+    res.json(apiData);
+  });
+
+// app.get("/api/get-quiz-questions", (req, res) => {
+//     return res.json(apiData);
+// });
+
+app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
