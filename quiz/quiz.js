89cebe0ac3342354d4bbead0a032ed112437e19f
Annu Poddar <annupoddar444@gmail.com>
2023-11-27 18:59:52 +0530

    Progressbar added to show the final result

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,14 +1,4 @@
 const container = document.querySelector('.container');
-const tasks = document.querySelector('.task');
-// const prevBtn = document.getElementById('prev');
-// const nextBtn = document.getElementById('next');
-// const submitBtn = document.getElementById('submit');
-// const userMessage = document.getElementById('msg');
-// // const listDiv = document.querySelector('.list');
-// const number = document.getElementById('number');
-// const quesNumQueue = document.querySelector('.disp_num');
-// const timer = document.querySelector('.timer');
-
 const questionDiv = document.querySelector('.question');
 const optionsDiv = document.querySelector('.option_list');
 const box = document.querySelector('.box');
@@ -21,24 +11,33 @@ const submitButton = document.getElementById('submit');
 const gameOverDiv = document.querySelector('.game_over');
 const reStartQuiz = document.getElementById('retake_quiz');
 const myScore = document.getElementById('myscore');
+const progressBar = document.querySelector('progress_bar');
+const down = document.querySelector('.down');
+const element = document.querySelector(".pagination ul");
+
+
 // const questionNumbers = document.querySelector('.question_numbers')
 
 let countIndex = 0;
+let totalScore = 0;
 let userSelectedOptns = {};
 let quizQuestions;
-
-const element = document.querySelector(".pagination ul");
+let timerInterval;
 let totalPages = 10;
 let page = 1;
+let play = true;
 
-startGame();
+if(play) {
+  startGame();
+}
+
 
 async function quizApi() {
     const response = await fetch('https://annu1245.github.io/quiz-api/data.json')
     quizQuestions = await response.json();
 }
 
-
+// This startGame function will fetching api
 function startGame() {
     box.style.display = 'none';
     gameOverDiv.style.display = 'none';
@@ -46,26 +45,21 @@ function startGame() {
     quizApi();
 }
 
-let cc = 10;
-let p = 3
+// This is a start button which start the game and set the timer
 startButton.addEventListener("click", () => {
     container.removeAttribute('id');
     startQuiz.style.display = 'none';
     box.style.display = 'block';
     startTimer();
     checkNextQuiz();
-
 })
 
-
-
-
-// quizApi();
-
+// this will check for the next quiz
 nextButton.addEventListener("click", () => {
     checkNextQuiz();
 })
 
+// this will check the previous quiz when user clicks on previous button
 previousButton.addEventListener("click", ()=>{
     if(countIndex > 1) {
         countIndex--;
@@ -75,6 +69,37 @@ previousButton.addEventListener("click", ()=>{
     }
 })
 
+// this will terminate the game and call show the result
+submitButton.addEventListener("click", () => {
+  clearInterval(timerInterval);
+    totalScore = calculateTotalScore();
+    box.style.display = 'none';
+    container.setAttribute('id', 'center'); 
+    gameOverDiv.style.display = 'block';
+    myScore.innerHTML = `${totalScore * 10} %`;
+    let resultText;
+    if(totalScore < 7) {
+      resultText = `<p style="color:red ;">oppss you are Failed !!<p>
+                    <p> Better luck next time... </P>`
+    }else if(totalScore >= 7){
+      resultText = `<p style="color:green ;">Congratulationss !!!!<p>
+                    <p> You passed the Test </P>`
+    }
+   down.insertAdjacentHTML('afterbegin', resultText);
+})
+
+// This button is click to restart the quiz once result is displayed
+reStartQuiz.addEventListener("click", () => {
+    console.log("yes");
+    container.removeAttribute('id');
+    gameOverDiv.style.display = 'none';
+    countIndex = 0;
+    userSelectedOptns = {};
+    box.style.display = 'block';
+    startButton.click();
+})
+
+// This function is called when user clicks on next button this will check for the next quiz
 function checkNextQuiz() {
     if(countIndex < quizQuestions.length) {
         countIndex++;
@@ -87,6 +112,7 @@ function checkNextQuiz() {
     }
 }
 
+// This function will hide the previous button when user is on 1st quiz count
 function showPreviousButton() {
     if(countIndex === 1 || countIndex === 0) {
         previousButton.style.visibility = 'hidden';
@@ -95,12 +121,11 @@ function showPreviousButton() {
     }
 }
 
+// This function will dynamically create the quiz content by taking the count of quiz question
 function displayQuiz(currentData) {
     if(currentData === undefined || currentData == {}) {
         return;
-    }
-    console.log("cc", countIndex);
-    
+    }    
     showPreviousButton();
     questionDiv.innerHTML = `Q${countIndex} ${currentData.question}`
     currentData.options.map((optionValue) => {
@@ -113,14 +138,15 @@ function displayQuiz(currentData) {
     selectOptionListener();
 }
 
+// This function is used to clear the already existing question on the html page
 function clearPreviousElement() {
     questionDiv.innerHTML = '';
     optionsDiv.innerHTML = '';
 }
 
+// This will add listener on the options of question
 function selectOptionListener() {
     // Check if previously answerd
-    // previousSelectedOtpn();
     let optnArr = document.querySelectorAll('.options');
     optnArr.forEach((optn) => {
         optn.addEventListener('click', ()=>{
@@ -135,12 +161,13 @@ function selectOptionListener() {
     })
 }
 
-
+//this will update the object wich is used to store the answers of user
 function setUserAnswer(value) {
     userSelectedOptns[countIndex] = value;
     // update question number queue   
 }
 
+// This function is used to check whether the user already select the options displayd in the page 
 function previousSelectedOtpn() {
     let allOptn = document.querySelectorAll('.options');
     if(userSelectedOptns[countIndex] != undefined) {
@@ -152,8 +179,34 @@ function previousSelectedOtpn() {
     }
 }
 
-let totalScore = 0;
+// This will the show the score percent on the progressbar
+function runProgressBar(percent) {
+  if(percent == 0) {
+    document.querySelector(".circular-progress").style.background = `
+    conic-gradient(#FF0000 0deg, #fff 0deg)`;
+    return
+  }else {
 
+  let progressStartValue = 0;
+  let progressStartEnd = `${percent}`
+  let speed = 10;
+  let color = 'cyan';
+
+  let progessTimer = setInterval(() => {
+    progressStartValue++;
+    if (progressStartValue == progressStartEnd) {
+      clearInterval(progessTimer);
+    }
+    
+    document.querySelector(".circular-progress").style.background = `
+    conic-gradient(${color} ${3.6 * progressStartValue}deg, #fff 0deg)`;
+
+    document.querySelector(".course-value").innerHTML = progressStartValue + "%";
+  },speed);
+}
+}
+
+// This function is used to calculate the total score
 function calculateTotalScore() {
     let score = 0;
     for(const key in userSelectedOptns){
@@ -163,136 +216,18 @@ function calculateTotalScore() {
             score++;
         }
     }
+
+    runProgressBar(score * 10)
     return score;
 }
 
-submitButton.addEventListener("click", () => {
-    totalScore = calculateTotalScore();
-    box.style.display = 'none';
-    container.setAttribute('id', 'center');  
-    myScore.innerHTML = `${totalScore}`;
-    gameOverDiv.style.display = 'block';
-})
-
-reStartQuiz.addEventListener("click", () => {
-    console.log("yes");
-    container.removeAttribute('id');
-    gameOverDiv.style.display = 'none';
-    countIndex = 0;
-    userSelectedOptns = {};
-    box.style.display = 'block';
-    checkNextQuiz();
-})
-
-
-// // Game restart Button
-// const rePlay = document.createElement('button');
-
-
-// let countIndex = 0;
-// let quizList;
-
-
-// quizApi();
-// // show all question number in a queue to the top of page
-// function quesNumbers() {
-//     for(let i=1; i<=quizQuestions.length; i++) {
-//         let quesNum = document.createElement('span');
-//         quesNum.classList.add('ques_num_queue');
-//         quesNum.innerHTML = i;
-//         questionNumbers.appendChild(quesNum);
-//         // quesNumListener(quesNum);
-//     }
-//     questionNumberListener();
-// }
-
-// function questionNumberListener() {
-//     let allQuesNumArr = document.querySelectorAll('.ques_num_queue');
-//     allQuesNumArr.forEach((questionNumber) => {
-//         questionNumber.addEventListener("click", (e) => {
-//             //remove previous active
-//             allQuesNumArr.forEach(otherNumber => otherNumber.removeAttribute('id'));
-//             countIndex = parseInt(e.target.innerHTML);
-//             //set new to active
-//             e.target.setAttribute('id', 'current');
-//             clearPreviousElement();
-//             displayQuiz(quizQuestions[countIndex-1])
-//         })
-//     })
-// }
-
-
-
-// function quesNumListener(currQuesNumDiv) {
-//     currQuesNumDiv.addEventListener('click', () => {
-//         let currQuesNum = parseInt(currQuesNumDiv.innerHTML);
-//         let obj = quizList[currQuesNum-1];
-//         countIndex = currQuesNum;
-//         listDiv.innerHTML = '';
-//         displayQuiz(obj);
-//     })
-// }
-
-
-
-
-
-// function checkNextQuiz() {
-//     console.log("yes", countIndex);
-//     if(countIndex < quizList.length) {
-//         const currentData = quizList[countIndex];
-//         countIndex++;
-//         displayQuiz(currentData);
-//     }else {
-//         message("No more quiz.. Click on Submit")
-//     }
-// }
-
-
-
-
-
-// function updateQuestionNumQueue() {
-//     let userSelectedQuesNum = Object.keys(userSelectedOptns);
-//     let quesNumberQueue = document.querySelectorAll('.ques_num_queue');
-//     quesNumberQueue.forEach(quesNumber => {
-//         if(userSelectedQuesNum.includes(quesNumber.innerHTML)) {
-//             quesNumber.style.backgroundColor = 'tomato';
-//         }
-//     })
-// }
-
-
-
-
-
-// function showFooterButton() {
-//     submitBtn.style.visibility = 'visible';
-//     if(countIndex === 1) {
-//         prevBtn.style.visibility = 'hidden';
-//     }else{
-//         prevBtn.style.visibility = 'visible';
-//     }
-// }
-
-
-
-
-
-// rePlay.addEventListener("click", () => {
-//     countIndex = 0;
-//     totalScore = 0;
-//     userSelectedOptns = {};
-//     listDiv.innerHTML = '';
-//     number.style.display = 'block';
-//     checkNextQuiz();
-// })
-
+// Timer function
 function startTimer() {
     let time = 300; // 5 minutes in seconds
+    timer.innerHTML = `5:00`;
     const timerElement = document.getElementById('timer');
 
-    const timerInterval = setInterval(() => {
+        timerInterval = setInterval(() => {
         const minutes = Math.floor(time / 60);
         let seconds = time % 60;
 
@@ -314,22 +249,10 @@ function startTimer() {
 
 
 
-// // window.onload = function () {
-// //     quesNumbers();
-// //     checkNextQuiz();
-// //     prevBtn.style.visibility = 'hidden';
-// //     submitBtn.style.visibility = 'hidden';
-// // }
-
-
-//pagination 
-// selecting required element
 
 element.innerHTML = createPagination(totalPages = 10, page);
 //calling function with passing parameters and adding inside element which is ul tag
 function createPagination(totalPages, page){
-
-
 console.log("page", page);
 
   let liTag = '';
